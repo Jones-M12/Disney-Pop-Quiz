f88db73765fe8c508e6411a08bb328d0106a241f
Jones-M <61522666+Jones-M12@users.noreply.github.com>
2020-04-04 14:59:11 -0400

    homepage.html

---
+++ script.js
@@ -0,0 +1,50 @@
+// var startQuiz = document.querySelector("#start");
+
+// var 
+
+// var currentPlace=0
+
+// function getQuestions(){
+
+//     quizQuestions[currentPlace].text
+
+//  var quizQuestions= [  
+    
+//    { 
+//         text:"What animal was Tarzan raise by?",
+//         answers: ["penguin","elephant","gorillas","lions"],
+//         correctAnswer: "gorillas"
+//         if (text = this.correctAnswer){
+//             alert("correct " + this.correctAnswer),
+//         }
+//         else {
+//             alert("wrong answer");
+//         }
+// },
+
+//     "Question 2": "What is the name of princess Jasmine's pet?",
+//     answers:[""]
+
+//     "Question 3": "Who dod Pochaontas father want her to marry?",
+
+//     "Question 4": "Who is Mushu?",
+
+//     "Question 5": "Name the tea cup from Beauty & the Beast?",
+
+//     "Question 6": "What is the name of the child who owns Woody from Toy Story?",
+
+//  ]
+
+// quizQuestions[currentPlace]
+// quizQuestions.Question1
+
+// for(i=0;i<quizQuestions.length;i++){
+//     quizQuestions[i]
+// }
+
+
+
+
+
+
+// startQuiz.addEventListener("click", getQuestions);
